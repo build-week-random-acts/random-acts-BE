650572a8d81b9317188359baba6b894cb21007ce
Zurglots <bfeole@gmail.com>
2019-08-01 14:22:42 -0400

    Added unit testing for quote services

--- StartHere/src/test/java/com/randomacts/domain/services/QuoteServiceImplTest.java
+++ StartHere/src/test/java/com/randomacts/domain/services/QuoteServiceImplTest.java
@@ -1,44 +1,102 @@
 package com.randomacts.domain.services;
 
 import com.randomacts.domain.RandomActsApplication;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.randomacts.domain.config.SecurityConfig;
+import com.randomacts.domain.exceptions.ResourceNotFoundException;
+import com.randomacts.domain.models.Quote;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.persistence.EntityNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RandomActsApplication.class)
-class QuoteServiceImplTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class QuoteServiceImplTest
 {
 
-    @BeforeEach
-    public void setUp() throws Exception
+    @Autowired
+    private QuoteService quoteService;
+
+//    private MockMvc mockMvc;
+
+//    @Bean
+//    public Authentication authentication() {
+//        Collection<GrantedAuthority> authorities = new ArrayList<>();
+//        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+//        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("customUsername", "customPassword", authorities); return authentication; }
+//
+//    @Autowired
+//    private Authentication authentication;
+
+    @Before
+    public void AsetUp() throws Exception
     {
         MockitoAnnotations.initMocks(this);
     }
 
-    @AfterEach
-    void tearDown()
+    @After
+    public void BtearDown() throws Exception
     {
     }
 
     @Test
-    void delete()
+    public void Csave()
     {
+        Quote q2 = new Quote("Save this quote");
+
+        Quote saveQuote = quoteService.save(q2);
+
+        assertNotNull(saveQuote);
+
+        Quote foundQuote = quoteService.findQuoteById(saveQuote.getQuotesid());
+        assertEquals(saveQuote.getQuote(), foundQuote.getQuote());
     }
 
     @Test
-    void save()
+    public void Dupdate()
     {
+        Quote q1 = new Quote("a new quote");
+        q1.setQuotesid(80);
+
+        Quote updatedQuote = quoteService.update(q1, 6);
+
+        assertEquals("a new quote", updatedQuote.getQuote());
     }
 
-    @Test
-    void update()
+    @Test (expected = ResourceNotFoundException.class)
+    public void EdeleteNotFound()
     {
+        quoteService.delete(3000);
+        assertEquals(30, quoteService.findAll().size());
+    }
+
+//    @WithMockUser(roles = "ADMIN")
+    @Test
+    public void FdeleteFound()
+    {
+        quoteService.delete(6);
+        assertEquals(29, quoteService.findAll().size());
     }
 }
